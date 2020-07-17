87db623fa613a95381fe9a2398c6627c4cd9dfcc
hyunbin1 <kimgusqls1@gmail.com>
2020-07-17 18:39:31 +0900

    부연 설명

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-# ! ruquset & BeautifulSoup 변수 만들어 주기
+# ! request & BeautifulSoup 변수 만들어 주기
 
 # 1. request 변수
 
@@ -31,19 +31,39 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 # ? 우리가 가져올 정보는 이 중 page의 개수이다. 
 # pagination = 페이지 목록 
-# links = 리스트
+# ! links = anchor(앵커 = 페이지 틀) = 리스트 만들기 
 # ? pages 만 출력하기 = 페이지 수
 
 pagination = indeed_soup.find("div", {"class":"pagination"})
 links = pagination.find_all('a')
 
 
-#pages 리스트 만들기
-pages = []
-for link in links:
-    pages.append(link.find("span"))
+# pages 리스트 만들기
+# anchor가 있고, 이 요소안에 다른요소가 있고 그 요소에 string이 오직 하나 있다면
+# 그냥 anchor에서 string method를 실행하면 된다. beautiful soup이 알아서 찾아줌
 
-#마지막 span 빼주기
-# -1은 마지막에서부터 시작해서 첫 item을 나타낸다.
-pages = pages[:-1]
+
+pages = []
+#마지막 span 빼주기 = ##[-1]## 넣어주기 
+# ! -1은 마지막에서부터 시작해서 첫 item을 나타낸다.
+# 나중에 pages[:-1]을 해주게 되면 리스트에서 문자열을 숫자열로 바꿀때 
+# 페이지 수  추출 전이기 때문에 다른 부분이 숫자열이 아니라고 에러가 뜬다 
+for link in links[:-1]:
+    # ? 텍스트만 가져오기 = 마지막에 string 넣어주기
+    ## pages.append(link.find("span").string) ## 
+    ## == pages.append(link.string) = anchor에 들어있던 리스트에서 문자열만 가져오기 ##
+    # ? 페이지 숫자를 문자열로 가져옴 = 문자열을 숫자열로 바꿔주기
+    ## pages.append(link.string) ##
+    pages.append(int(link.string))
+
+# ? 페이지 수 중에서 가장 큰 수 찾기
+max_page = pages[-1] 
+
+# ? 페이지를 계속해서 request 하는 방법을 찾아야 된다. 
+# ? 5개의 request를 만들어야 됨.
+
+
+
 print(pages)
+
+ 
